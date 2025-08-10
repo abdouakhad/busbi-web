526e9543c7cd27e4ca287365d1755fae52a801e1
akhad <akhad0015gmail.com>
2025-08-10 11:32:08 +0000

    added carpooling page

---
+++ app/carpooling/page.js
@@ -0,0 +1,9 @@
+export default function CarpoolingPage() {
+  return (
+    <div>
+      <h1>Carpooling</h1>
+      <p>Download our mobile app for the best experience!</p>
+      {/* Add app store links, web version of carpooling, etc. */}
+    </div>
+  );
+}
